833f880bc706f5d3f824a8aa281cc02da0d447d6
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2023-12-14 01:21:29 +0200

    add client id

--- backend.tf
+++ backend.tf
@@ -5,6 +5,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
+    client_id            = "fe8a157b-8c95-4fef-aa9c-4cdf8aa020cb"
     subscription_id      = "51920f08-3f01-4924-897e-c64491914947"
     tenant_id            = "ea664a21-ac74-4f72-b086-cd4820bf5518"
   }
