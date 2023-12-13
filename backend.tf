9b0b60afc8a87a3bbc0571b34d7d6d340a98c724
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2023-12-14 01:19:36 +0200

    hardcode id's

--- backend.tf
+++ backend.tf
@@ -5,5 +5,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
+    subscription_id      = "51920f08-3f01-4924-897e-c64491914947"
+    tenant_id            = "ea664a21-ac74-4f72-b086-cd4820bf5518"
   }
 }
